bff51bc28fdf1bbe803e648f50db3bffb847d4ce
Rafl27 <raflsouza27@gmail.com>
2022-07-05 11:35:10 -0300

    Child to Parent

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,20 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    //gets the expense data from the prop, copies it and adds an id to it.
+    const saveExpenseData = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    }
+
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            {/* on is just a convention for such type of prop */}
+            <ExpenseForm onSaveExpenseData={saveExpenseData} />
         </div>
     )
 }
